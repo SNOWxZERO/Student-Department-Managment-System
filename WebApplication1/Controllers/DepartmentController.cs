f4430aae3cb52100751e5ed39798ef4867a2eb8b
SNOWxZERO <snowxzero@gmail.com>
2022-06-06 17:34:04 +0200

    Add project files.

---
+++ WebApplication1/Controllers/DepartmentController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        SchoolEntities1 db = new SchoolEntities1();
+        // GET: Department
+        public ActionResult Index()
+        {
+            List<Department> dpt = db.Department.ToList<Department>();
+
+            return View(dpt);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(int DeptID, string Deptname, int Capacity)
+        {
+            Department dpt = new Department() { DeptID = DeptID, Deptname = Deptname , Capacity = Capacity};
+            db.Department.Add(dpt);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Department dpt = db.Department.Find(id);
+            return View(dpt);
+        }
+        [HttpPost]
+        public ActionResult Edit(Department dpt)
+        {
+            Department s = db.Department.Find(dpt.DeptID);
+            s.DeptID = dpt.DeptID;
+            s.Deptname = dpt.Deptname;
+            s.Capacity = dpt.Capacity;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Remove(int id)
+        {
+            Department dpt = db.Department.Find(id);
+            return View(dpt);
+        }
+
+
+        public ActionResult RemoveConfirmed(int id)
+        {
+            Department s = db.Department.Find(id);
+            db.Department.Remove(s);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(401, "Id must have a value");
+            Department dpt = db.Department.Find(id);
+            if (dpt == null)
+                return HttpNotFound();
+            return View(dpt);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        SchoolEntities1 db = new SchoolEntities1();
+
+        public ActionResult Index()
+        {
+            List<Student> std = db.Student.ToList<Student>();
+
+            return View(std);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(string name, int id,string mail,string phon,int dep_id)
+        {
+            Student std = new Student() { name = name, id = id,mail=mail,phon= phon,dep_id=dep_id };
+            db.Student.Add(std);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Student std = db.Student.Find(id);
+            return View(std);
+        }
+        [HttpPost]
+        public ActionResult Edit(Student std)
+        {
+            Student s = db.Student.Find(std.id);
+            s.name = std.name;
+            s.id = std.id;
+            s.mail = std.mail;
+            s.phon = std.phon;
+            s.dep_id = std.dep_id;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Remove(int id)
+        {
+            Student std = db.Student.Find(id);
+            return View(std);
+        }
+        
+
+        public ActionResult RemoveConfirmed(int id)
+        {
+            Student s = db.Student.Find(id);
+            db.Student.Remove(s);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(401, "ID must have a value");
+            Student std = db.Student.Find(id);
+            if (std == null)
+                return HttpNotFound();
+            return View(std);
+        }
+    }
+}
